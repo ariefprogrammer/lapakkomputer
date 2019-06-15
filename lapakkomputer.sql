4ea4be04ad59550561e042f19b17add6bdd6a4e9
ariefhdt <blackconnor78@gmail.com>
2019-06-15 10:27:00 +0700

    update the databases

--- lapakkomputer.sql
+++ lapakkomputer.sql
@@ -1,85 +1,114 @@
-/*
-SQLyog Ultimate v12.09 (64 bit)
-MySQL - 10.1.26-MariaDB : Database - lapakkomputer
-*********************************************************************
-*/
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 15 Jun 2019 pada 05.25
+-- Versi Server: 10.1.26-MariaDB
+-- PHP Version: 7.1.8
 
-/*!40101 SET NAMES utf8 */;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-/*!40101 SET SQL_MODE=''*/;
 
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`lapakkomputer` /*!40100 DEFAULT CHARACTER SET latin1 */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-USE `lapakkomputer`;
+--
+-- Database: `lapakkomputer`
+--
 
-/*Table structure for table `coba` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `coba`;
+--
+-- Struktur dari tabel `coba`
+--
 
 CREATE TABLE `coba` (
   `coba_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Data for the table `coba` */
+-- --------------------------------------------------------
 
-/*Table structure for table `kondisi_products` */
-
-DROP TABLE IF EXISTS `kondisi_products`;
+--
+-- Struktur dari tabel `kondisi_products`
+--
 
 CREATE TABLE `kondisi_products` (
-  `kondisi_id` int(11) NOT NULL AUTO_INCREMENT,
-  `nama_kondisi` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`kondisi_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `kondisi_id` int(11) NOT NULL,
+  `nama_kondisi` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Data for the table `kondisi_products` */
+--
+-- Dumping data untuk tabel `kondisi_products`
+--
 
-insert  into `kondisi_products`(`kondisi_id`,`nama_kondisi`) values (1,'Baru'),(2,'Bekas');
+INSERT INTO `kondisi_products` (`kondisi_id`, `nama_kondisi`) VALUES
+(1, 'Baru'),
+(2, 'Bekas');
 
-/*Table structure for table `pelanggan` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `pelanggan`;
+--
+-- Struktur dari tabel `pelanggan`
+--
 
 CREATE TABLE `pelanggan` (
-  `pelanggan_id` int(10) NOT NULL AUTO_INCREMENT,
+  `pelanggan_id` int(10) NOT NULL,
   `nama_store` varchar(255) DEFAULT NULL,
   `alamat_store` varchar(255) DEFAULT NULL,
   `email_store` varchar(255) DEFAULT NULL,
-  `telepon_store` decimal(13,0) DEFAULT NULL,
-  PRIMARY KEY (`pelanggan_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+  `telepon_store` decimal(13,0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Data for the table `pelanggan` */
+--
+-- Dumping data untuk tabel `pelanggan`
+--
 
-insert  into `pelanggan`(`pelanggan_id`,`nama_store`,`alamat_store`,`email_store`,`telepon_store`) values (5,'Abid Alfatir Comp','Sleman, Yogyakarta','abid@gmail.com','81308130813'),(6,'Ahmad Subarjo Comp','Jl Tengiri V No 43 Minosarmoni, Ngaglagia, Slemus, Yogyakarta','ahmadsubarjo@gmail.com','81308130813'),(7,'Surya Kencana Comp','Jl AM Sangaji, Yogyakarta','suryakencana@gmail.com','81308130813'),(8,'Ardi Comp','Jl Tirowardi, Raman Utara, Lampung Tengah.','ardicom78@gmail.com','81346538756'),(9,'Arief Store','Jl Wahid Hasyim','hdt@gmail.com','81308130813');
+INSERT INTO `pelanggan` (`pelanggan_id`, `nama_store`, `alamat_store`, `email_store`, `telepon_store`) VALUES
+(5, 'Abid Alfatir Comp', 'Sleman, Yogyakarta', 'abid@gmail.com', '81308130813'),
+(6, 'Ahmad Subarjo Comp', 'Jl Tengiri V No 43 Minosarmoni, Ngaglagia, Slemus, Yogyakarta', 'ahmadsubarjo@gmail.com', '81308130813'),
+(7, 'Surya Kencana Comp', 'Jl AM Sangaji, Yogyakarta', 'suryakencana@gmail.com', '81308130813'),
+(8, 'Ardi Comp', 'Jl Tirowardi, Raman Utara, Lampung Tengah.', 'ardicom78@gmail.com', '81346538756'),
+(9, 'Arief Store', 'Jl Wahid Hasyim', 'hdt@gmail.com', '81308130813'),
+(10, 'Suryana Comp', 'Jl Tenggolong raya, No 43. Sleman, Yogyakarta', 'suryana@gmail.com', '81308130813');
 
-/*Table structure for table `pembelian` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `pembelian`;
+--
+-- Struktur dari tabel `pembelian`
+--
 
 CREATE TABLE `pembelian` (
-  `pembelian_id` int(64) NOT NULL AUTO_INCREMENT,
+  `pembelian_id` int(64) NOT NULL,
   `product_id` int(64) DEFAULT NULL,
   `pelanggan_id` int(64) DEFAULT NULL,
   `jumlah_pembelian` int(15) DEFAULT NULL,
-  `tanggal_pembelian` date DEFAULT NULL,
-  PRIMARY KEY (`pembelian_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `tanggal_pembelian` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Data for the table `pembelian` */
+--
+-- Dumping data untuk tabel `pembelian`
+--
 
-insert  into `pembelian`(`pembelian_id`,`product_id`,`pelanggan_id`,`jumlah_pembelian`,`tanggal_pembelian`) values (1,3,5,1,'2019-05-10'),(2,4,6,12,'2019-05-14'),(3,3,5,12,'2019-05-16');
+INSERT INTO `pembelian` (`pembelian_id`, `product_id`, `pelanggan_id`, `jumlah_pembelian`, `tanggal_pembelian`) VALUES
+(1, 3, 5, 1, '2019-05-10'),
+(2, 4, 6, 12, '2019-05-14'),
+(6, 5, 5, 22222, '2019-06-06'),
+(13, 5, 7, 12, '2017-12-12');
 
-/*Table structure for table `products` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `products`;
+--
+-- Struktur dari tabel `products`
+--
 
 CREATE TABLE `products` (
-  `product_id` int(10) NOT NULL AUTO_INCREMENT,
+  `product_id` int(10) NOT NULL,
   `nama_product` varchar(255) DEFAULT NULL,
   `brand_product` varchar(255) DEFAULT NULL,
   `harga_product` int(15) DEFAULT NULL,
@@ -89,15 +118,72 @@ CREATE TABLE `products` (
   `kondisi_id` varchar(10) DEFAULT NULL,
   `berat_product` int(11) DEFAULT NULL,
   `foto_product` varchar(255) DEFAULT NULL,
-  `deskripsi_product` text,
-  PRIMARY KEY (`product_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+  `deskripsi_product` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Data for the table `products` */
+--
+-- Dumping data untuk tabel `products`
+--
 
-insert  into `products`(`product_id`,`nama_product`,`brand_product`,`harga_product`,`minimal_beli`,`qty_product`,`warna_tersedia`,`kondisi_id`,`berat_product`,`foto_product`,`deskripsi_product`) values (3,'LENOVO IdeaPad 720S 13IKB - I5 8250 8GB','Lenovo',9500000,6,62,'Grey, Hitam','1',2310,'3.png','Lenovo product description'),(4,'Xiaomi Mi Notebook Pro i7 16GB 256GB','xiaomi',12500000,12,75,'Merah, Hitam, Silver','1',1356,'.jpg','Xiaomi description'),(5,'Macbook Pro 2018 256 GB','Apple',28325000,12,86,'grey','1',1250,'5.jpg','Macbook pro 2018 ini adalah laptop yang memiliki kapasitas RAM 8GB dan mempunyai kecepatan tinggi dalam melakukan banyak operasi.'),(9,'Asus ROG 520 GB RAM','Asus',28575000,12,45,'Hitam','1',1234,'Asus_ROG_520_GB_RAM.jpg','Asus ROG adalah laptop yang memiliki RAM 8 GB');
+INSERT INTO `products` (`product_id`, `nama_product`, `brand_product`, `harga_product`, `minimal_beli`, `qty_product`, `warna_tersedia`, `kondisi_id`, `berat_product`, `foto_product`, `deskripsi_product`) VALUES
+(3, 'LENOVO IdeaPad 720S 13IKB - I5 8250 8GB', 'Lenovo', 9500000, 6, 62, 'Grey, Hitam', '1', 2310, '3.png', 'Lenovo product description'),
+(4, 'Xiaomi Mi Notebook Pro i7 16GB 256GB', 'xiaomi', 12500000, 12, 75, 'Merah, Hitam, Silver', '1', 1356, '.jpg', 'Xiaomi description'),
+(5, 'Macbook Pro 2018 256 GB', 'Apple', 28325000, 12, 86, 'grey', '1', 1250, '5.jpg', 'Macbook pro 2018 ini adalah laptop yang memiliki kapasitas RAM 8GB dan mempunyai kecepatan tinggi dalam melakukan banyak operasi.'),
+(9, 'Asus ROG 520 GB RAM', 'Asus', 28575000, 12, 45, 'Hitam', '1', 1234, 'Asus_ROG_520_GB_RAM.jpg', 'Asus ROG adalah laptop yang memiliki RAM 8 GB, lorem ipsum');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `kondisi_products`
+--
+ALTER TABLE `kondisi_products`
+  ADD PRIMARY KEY (`kondisi_id`);
+
+--
+-- Indexes for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`pelanggan_id`);
+
+--
+-- Indexes for table `pembelian`
+--
+ALTER TABLE `pembelian`
+  ADD PRIMARY KEY (`pembelian_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `kondisi_products`
+--
+ALTER TABLE `kondisi_products`
+  MODIFY `kondisi_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  MODIFY `pelanggan_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `pembelian`
+--
+ALTER TABLE `pembelian`
+  MODIFY `pembelian_id` int(64) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `product_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
